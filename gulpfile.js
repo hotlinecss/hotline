3cdcd8419a04e22f4d3d0953e0cef9036412e99a
Francisco <andrewplanet@gmail.com>
2016-12-27 22:55:21 -0400

    Some changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,36 @@
-var gulp   = require("gulp");
-var sass   = require("gulp-sass");
-var size   = require("gulp-size");
-var clean  = require("gulp-clean-css");
-var header = require("gulp-header")
-var rename = require("gulp-rename");
-var prefix = require("gulp-autoprefixer");
-var pkg    = require("./package.json");
+const gulp   = require("gulp");
+const sass   = require("gulp-sass");
+const size   = require("gulp-size");
+const clean  = require("gulp-clean-css");
+const header = require("gulp-header")
+const rename = require("gulp-rename");
+const prefix = require("gulp-autoprefixer");
+const pkg    = require("./package.json");
 
-gulp.task("css", function() {
-	return gulp.src("./src/*.scss")
+const options = {
+	"paths": {
+		"srcAll": "./src/*.scss",
+		"src": "./src/xvx.scss",
+		"dist": "./css/",
+		"root": "./"
+	},
+	"prefix": ["last 2 versions"]
+}
+
+gulp.task("build:css", function() {
+	return gulp.src(options.paths.srcAll)
 		.pipe(sass().on("error", sass.logError))
-		.pipe(prefix({browsers: ["last 2 versions"]}))
+		.pipe(prefix({browsers: options.prefix }))
 		.pipe(header("/* ${pkg.name} - ${pkg.version} */\n", {pkg: pkg}))
-		.pipe(gulp.dest("./dist/"))
+		.pipe(gulp.dest(options.paths.dist))
 		.pipe(size({title: "Raw: "}))
 		.pipe(clean())
 		.pipe(rename({suffix: ".min"}))
 		.pipe(header("/* ${pkg.name} - ${pkg.version} */\n", {pkg: pkg}))
 		.pipe(size({title: "Min: "}))
-		.pipe(gulp.dest("./dist/"))
+		.pipe(gulp.dest(options.paths.dist))
 });
 
-gulp.task("default", ["css"], function() {
-	gulp.watch("./src/**/*.scss", ["css"])
+gulp.task("default", ["build:css"], function() {
+	gulp.watch("./src/**/*.scss", ["build:css"])
 })
