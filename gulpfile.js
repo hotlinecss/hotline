fcdc1ad28030bdbc0753eea7be7d437a06560d4e
Francisco <andrewplanet@gmail.com>
2017-01-01 15:49:57 -0400

    Changed the proposite of the project, added a lot of new devdeps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-const gulp   = require("gulp");
-const sass   = require("gulp-sass");
-const size   = require("gulp-size");
-const clean  = require("gulp-clean-css");
-const header = require("gulp-header")
-const rename = require("gulp-rename");
-const prefix = require("gulp-autoprefixer");
-const pkg    = require("./package.json");
+const gulp    = require("gulp");
+const postcss = require("gulp-postcss");
+const sass    = require("gulp-sass");
+const mq      = require("css-mqpacker");
+const size    = require("gulp-size");
+const clean   = require("gulp-clean-css");
+const header  = require("gulp-header")
+const rename  = require("gulp-rename");
+const prefix  = require("gulp-autoprefixer");
+const pkg     = require("./package.json");
 
 const options = {
 	"paths": {
@@ -14,23 +16,26 @@ const options = {
 		"dist": "./css/",
 		"root": "./"
 	},
-	"prefix": ["last 2 versions"]
+	"prefix": ["last 2 versions"],
+	"processors": [mq()]
 }
 
 gulp.task("build:css", function() {
 	return gulp.src(options.paths.srcAll)
-		.pipe(sass().on("error", sass.logError))
+		.pipe(sass({outputStyle: "expanded"}).on("error", sass.logError))
 		.pipe(prefix({browsers: options.prefix }))
-		.pipe(header("/* ${pkg.name} - ${pkg.version} */\n", {pkg: pkg}))
+		.pipe(postcss(options.processors))
+		.pipe(header("/* ${pkg.name} - ${pkg.version} - ${pkg.license} */\t", {pkg: pkg}))
 		.pipe(gulp.dest(options.paths.dist))
 		.pipe(size({title: "Raw: "}))
 		.pipe(clean())
 		.pipe(rename({suffix: ".min"}))
-		.pipe(header("/* ${pkg.name} - ${pkg.version} */\n", {pkg: pkg}))
+		.pipe(header("/* ${pkg.name} - ${pkg.version} - ${pkg.license} */\t", {pkg: pkg}))
 		.pipe(size({title: "Min: "}))
+		.pipe(size({title: "Gzip: ", gzip: true}))
 		.pipe(gulp.dest(options.paths.dist))
 });
 
 gulp.task("default", ["build:css"], function() {
-	gulp.watch("./src/**/*.scss", ["build:css"])
+	gulp.watch(options.paths.srcAll, ["build:css"])
 })
