7402368cc702c05ff771048a9b57d1376544d1a0
franciscogo <frandresgo@gmail.com>
2019-03-24 16:49:31 -0400

    Final

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp    		= require("gulp");
+const { series, src, dest, watch } = require('gulp');
 const postcss 		= require("gulp-postcss");
 const sass    		= require("gulp-sass");
 const mq      		= require("css-mqpacker");
@@ -10,43 +10,79 @@ const prefix  		= require("gulp-autoprefixer");
 const sourcemaps  = require("gulp-sourcemaps");
 const pkg         = require("./package.json");
 
-const options = {
-	"paths": {
-		"srcAll": "./src/*.scss",
-		"src": "./src/hotline.scss",
-		"dist": "./css/",
-		"root": "./"
-	},
-	"prefix": ["last 3 years", "not ie <= 9"],
-	"processors": [mq()]
+const configuration = {
+	rootFolder: './',
+	folderDest: './css/',
+	sassSrc: './src/*.scss',
+	fileDest: './css/hotline.css',
+	prefix: ["last 4 years", "not ie <= 9"]
 }
 
-var banner = ['/**',
+const banner = ['/**',
   ' * <%= pkg.name %> - <%= pkg.description %>',
   ' * @version v<%= pkg.version %>',
   ' * @link <%= pkg.homepage %>',
   ' * @license <%= pkg.license %>',
   ' */', '\n',
-  ''].join('\n');
+	''].join('\n');
 
-gulp.task("build:css", function() {
-	return gulp.src(options.paths.srcAll)
-		.pipe(sourcemaps.init())
-		.pipe(sass({outputStyle: "expanded"}).on("error", sass.logError))
-		.pipe(prefix({browsers: options.prefix }))
-		.pipe(postcss(options.processors))
-		.pipe(header(banner, {pkg: pkg}))
-		.pipe(sourcemaps.write(options.paths.root))
-		.pipe(gulp.dest(options.paths.dist))
-		.pipe(size({title: "Raw: "}))
+function _initSourcemaps (cb) {
+	return sourcemaps.init();
+}
+
+function _writeSourcemaps (cb) {
+	return sourcemaps.write(configuration.rootFolder);
+}
+
+function compileWithSass (cb) {
+	return src(configuration.sassSrc)
+		.pipe(_initSourcemaps())
+		.pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+		.pipe(size({ title: 'Expanded' }))
+		.pipe(_writeSourcemaps())
+		.pipe(dest(configuration.folderDest));
+}
+
+function addHeader (cb) {
+	return src(configuration.fileDest)
+		.pipe(header(banner, { pkg: pkg }))
+		.pipe(dest(configuration.folderDest));
+}
+
+function prefixCSS (cb) {
+	return src(configuration.fileDest)
+		.pipe(prefix({ browsers: configuration.prefix }))
+		.pipe(dest(configuration.folderDest));
+}
+
+function reduceMediaQueries (cb) {
+	return src(configuration.fileDest)
+		.pipe(postcss([mq()]))
+		.pipe(dest(configuration.folderDest));
+}
+
+function minifyAndRenameCSS (cb) {
+	return src(configuration.fileDest)
+		.pipe(_initSourcemaps())
 		.pipe(clean())
-		.pipe(rename({suffix: ".min"}))
-		.pipe(header(banner, {pkg: pkg}))
-		.pipe(size({title: "Min: "}))
-		.pipe(size({title: "Gzip: ", gzip: true}))
-		.pipe(gulp.dest(options.paths.dist))
-});
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(size({ title: 'Minify' }))
+		.pipe(size({ title: 'Minify (gzip)', gzip: true }))
+		.pipe(_writeSourcemaps())
+		.pipe(dest(configuration.folderDest));
+}
 
-gulp.task("default", ["build:css"], function() {
-	gulp.watch(options.paths.srcAll, ["build:css"]);
-})
+const tasks = series(
+	compileWithSass,
+	addHeader,
+	prefixCSS, 
+	reduceMediaQueries,
+	minifyAndRenameCSS
+)
+
+function watching () {
+	watch(configuration.sassSrc, tasks);
+}
+
+exports.default = series(tasks, watching);
+
